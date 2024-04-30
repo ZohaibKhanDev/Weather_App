ffdf39fb923347412b95ed8142cac414dd50048e
Muhammad Khubaib Imtiaz <98816544+KhubaibKhan4@users.noreply.github.com>
2024-04-30 12:04:46 +0500

    -Home Screen impl

--- app/src/main/java/com/example/weathers/presentation/ui/screen/LocationScreen.kt
+++ app/src/main/java/com/example/weathers/presentation/ui/screen/LocationScreen.kt
@@ -1,15 +1,20 @@
 package com.example.weathers.presentation.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -23,8 +28,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.weathers.R
 import com.example.weathers.domain.model.Weather.Current
 import com.example.weathers.domain.model.Weather.Weather
 import com.example.weathers.domain.usecase.ResultState
@@ -83,37 +93,50 @@ fun WeatherData(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0XFFFFFFFF)),
-        verticalArrangement = Arrangement.SpaceBetween,
+            .background(Color.White),
+        verticalArrangement = Arrangement.spacedBy(5.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Box(modifier = Modifier.width(287.dp).height(60.dp), contentAlignment = Alignment.TopCenter){
-            TextField(value = search,
-                onValueChange = {
-                    search = it
-                },
-                placeholder = {
-                    Text(text = "Search")
-                },
-                modifier = Modifier
-                    .fillMaxSize(),
-                colors = TextFieldDefaults.textFieldColors(
-                    focusedTextColor = Color(0XFFC4C4C4),
-                    unfocusedTextColor = Color(0XFFC4C4C4),
-                    focusedIndicatorColor = Color.White,
-                    unfocusedIndicatorColor = Color.White,
-                    focusedPlaceholderColor = Color(0XFFC4C4C4),
-                    unfocusedPlaceholderColor = Color(0XFFC4C4C4),
-                    focusedLeadingIconColor = Color(0XFFC4C4C4),
-                    unfocusedLeadingIconColor = Color(0XFFC4C4C4),
-                    focusedTrailingIconColor = Color(0XFFC4C4C4),
-                    unfocusedTrailingIconColor = Color(0XFFC4C4C4),
-                    containerColor = Color(0XFFFDFCFC)
-                ), shape = RoundedCornerShape(15.dp)
-            )
-        }
 
+        TextField(
+            value = search,
+            onValueChange = {
+                search = it
+            },
+            placeholder = {
+                Text(text = "Search location", fontSize = 14.sp, fontWeight = FontWeight.Bold)
+            },
+            modifier = Modifier
+                .width(287.dp)
+                .height(50.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                focusedTextColor = Color(0XFFC4C4C4),
+                unfocusedTextColor = Color(0XFFC4C4C4),
+                focusedIndicatorColor = Color.White,
+                unfocusedIndicatorColor = Color.White,
+                focusedPlaceholderColor = Color(0XFFC4C4C4),
+                unfocusedPlaceholderColor = Color(0XFFC4C4C4),
+                focusedLeadingIconColor = Color(0XFFC4C4C4),
+                unfocusedLeadingIconColor = Color(0XFFC4C4C4),
+                focusedTrailingIconColor = Color(0XFFC4C4C4),
+                unfocusedTrailingIconColor = Color(0XFFC4C4C4),
+                containerColor = Color(0XFFFDFCFC)
+            ), shape = RoundedCornerShape(30.dp), trailingIcon = {
+                Icon(imageVector = Icons.Outlined.Search, contentDescription = "")
+            }, textStyle = TextStyle(
+                fontSize = 14.sp, fontWeight = FontWeight.Bold
+            )
+        )
+
+
+
+
+        Image(
+            painter = painterResource(id = R.drawable.weather),
+            contentDescription = "",
+            modifier = Modifier.size(120.dp)
+        )
 
     }
 
